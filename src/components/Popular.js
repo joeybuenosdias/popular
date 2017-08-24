59ded2f57931420e8a88e229feb142b0e546f4cc
Joey Schrader <joedanproperty@gmail.com>
2017-08-24 17:57:41 -0600

    stateless component

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
-class Popular extends React.Component{
+const SelectLanguage = (props) => {
+  let languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
+  return(
+    <ul className='languages'>
+      {languages.map( (lang) => {
+        return (
+          <li
+            style={ lang === props.selectedLanguage ? {color: 'red'} : null }
+            key={lang}
+            onClick={props.onSelect.bind(null, lang)}
+            >
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+
+class Popular extends React.Component {
+
   state = { selectedLanguage: 'All' }
 
   updateLanguage = (lang) => {
@@ -9,24 +30,12 @@ class Popular extends React.Component{
 
   render(){
 
-    let languages = ['All','JavaScript','Ruby','Java','CSS','Python']
-
     return(
       <div>
-      <p>Selected Language: {this.state.selectedLanguage}</p>
-      <ul className='languages'>
-        {languages.map( (lang) => {
-          return (
-            <li
-              style={ lang === this.state.selectedLanguage ? {color: 'red'} : null }
-              key={lang}
-              onClick={this.updateLanguage.bind(null, lang)}
-              >
-              {lang}
-            </li>
-          )
-        })}
-      </ul>
+        <SelectLanguage
+          onSelect={this.updateLanguage}
+          selectedLanguage={this.state.selectedLanguage}
+        />
       </div>
     )
   }
