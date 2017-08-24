8ca46dd510c6474f17f2d063fd42d74f379d0b27
Joey Schrader <joedanproperty@gmail.com>
2017-08-24 17:24:01 -0600

    got navigation working

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 class Popular extends React.Component{
+  state = { selectedLanguage: 'All' }
+
+  updateLanguage = (lang) => {
+    return this.setState({ selectedLanguage: lang })
+  }
 
   render(){
 
     let languages = ['All','JavaScript','Ruby','Java','CSS','Python']
 
     return(
+      <div>
+      <p>Selected Language: {this.state.selectedLanguage}</p>
       <ul className='languages'>
         {languages.map( (lang) => {
           return (
-            <li>{lang}</li>
+            <li
+              key={lang}
+              onClick={this.updateLanguage.bind(null, lang)}
+              >
+              {lang}
+            </li>
           )
         })}
       </ul>
+      </div>
     )
   }
 }
